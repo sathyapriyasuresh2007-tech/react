65c3328a7b5f80c4cc9dc81b98a9bf75cc94c3e2
sathyapriyasuresh2007-tech <sathyapriyasuresh2007@gmail.com>
2026-06-27 13:40:19 +0530

    ready for deploy

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>🌍 Global Health & COVID-19 Tracker</h1>
+      <p>Real-time updates for health awareness campaigns</p>
+    </header>
+  );
+};
+
+export default Header;
